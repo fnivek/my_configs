7963ba5d530e253d8f6a5bf7ca2307dd2c4e9863
Kevin French <fnivek@gmail.com>
2019-05-09 10:49:03 -0400

    TERM_COLOR: Fix background color of ntfs file system

--- .bash/default.sh
+++ .bash/default.sh
@@ -74,7 +74,7 @@ esac
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    test -r ~/.bash/mydircolors && eval "$(dircolors -b ~/.bash/mydircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
