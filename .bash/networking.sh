bc04b1b97e3395846eb0aa8c71322f3b63984d0f
Kevin French <fnivek@gmail.com>
2020-03-24 09:34:19 -0400

    NETWORK: Add shortcuts for common networking task

--- .bash/networking.sh
+++ .bash/networking.sh
@@ -1,1 +1,17 @@
 alias cisco="/opt/cisco/anyconnect/bin/vpnui"
+
+function ssid-wifi() {
+  echo "$(nmcli -t -f active,ssid dev wifi | awk '/^yes:/' | awk -F ':' '{print $2}')"
+}
+
+function con-new-wifi() {
+  nmcli device wifi connect "$1"
+}
+
+function con-wifi() {
+  nmcli con up id "$1"
+}
+
+function list-cons() {
+  nmcli connection
+}
